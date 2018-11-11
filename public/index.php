9004807666080f0d0e83f7232d334a0cbc27bf16
crcms <crcms@crcms.cn>
2018-11-11 17:36:02 +0800

    init

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', 'On');
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+\CrCms\Microservice\Bootstrap\Start::run($_SERVER['argv'] ?? [], __DIR__.'/../');
