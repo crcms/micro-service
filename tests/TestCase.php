5d745f15f64efa69b986226fc07ea0f59602f75c
crcms <crcms@crcms.cn>
2018-11-18 17:15:16 +0800

    testing

---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @author simon <simon@crcms.cn>
+ * @datetime 2018-11-18 16:56
+ * @link http://crcms.cn/
+ * @copyright Copyright &copy; 2018 Rights Reserved CRCMS
+ */
+
+namespace CrCms\Tests;
+
+use CrCms\Microservice\Testing\TestCase as BaseTestCase;
+
+/**
+ * Class TestCase
+ * @package CrCms\Tests
+ */
+class TestCase extends BaseTestCase
+{
+
+}
